6c262478ac81bfd7cbb0359d5fa087f00e22154a
chaospat <patouk342@hotmail.fr>
2018-01-31 18:55:00 -0500

    Ajout d'un dialogue manager
    J'ai fait un Dialogue manager et un Canvas pour afficher des dialogue.
    J'ai également fait un peut de classement dans les fichiers

---
+++ Cuboid/Assets/Script/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    [Range(1f, 15f)]
+    public float smoothSpeed = 5f;
+
+    public Vector3 offset;
+
+    void FixedUpdate() {
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
