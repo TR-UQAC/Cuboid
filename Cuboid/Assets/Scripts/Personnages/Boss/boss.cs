819517ab60fe30fc1af338fddcdb33973e970cba
Gabriel-PC <gab_sim16@hotmail.com>
2018-03-12 12:54:46 -0400

    Ajout de plugin DOTween et début du boss
    J'ai commencer à travailler le boss et j'ai ajouter un plugin très utile pour faire des animation DOTween

---
+++ Cuboid/Assets/Scripts/Personnages/Boss/boss.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody2D))]
+public class boss : Personnages
+{
+
+    private void Awake()
+    {
+        //  set les variable avant d'être actif
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		//  lancé l'intro du boss
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void DommagePerso(int dommage)
+    {
+
+        /*if (!ennemiStats.immortel)
+        {
+            ennemiStats.immortel = true;
+            StartCoroutine(ChangeImmortel());
+            ennemiStats.vie -= dommage;
+
+            if (ennemiStats.vie <= 0)
+            {
+                GameMaster.KillEnnemi(this);
+                //TimeManager.DoSlowMotion();
+            }
+        }*/
+    }
+}
