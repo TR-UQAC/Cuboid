0f494de4770a30bbb363e15d8357ca4e21b00054
Gabriel-PC <gab_sim16@hotmail.com>
2018-03-15 22:02:08 -0400

    Correction du boss et avancement

--- Cuboid/Assets/Scripts/Personnages/Boss/TriggerBossStage.cs
+++ Cuboid/Assets/Scripts/Personnages/Boss/TriggerBossStage.cs
@@ -12,18 +12,23 @@ public class TriggerBossStage : MonoBehaviour {
     private bool m_bossActive = false;
     private bool m_bossFin = false;
 
+    private Vector3 m_lastPosBoss;
+
     // Use this for initialization
     void Start () {
         if (!m_boss)
             enabled = false;
 
         m_boss.SetActive(false);
+        m_boss.GetComponent<boss>().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(m_boss == null && m_bossFin == false)
         {
+            m_bossFin = true;
+            m_bossActive = false;
             //le boss est mort
             Debug.Log("LE BOSS EST MOURU");
 
@@ -33,11 +38,16 @@ public class TriggerBossStage : MonoBehaviour {
             //  faire pop l'upgrade
             Sequence loot = DOTween.Sequence();
 
-            loot.Append(m_upgrade.transform.DOMove(m_boss.transform.position, 0.01f));
+            loot.Append(m_upgrade.transform.DOMove(m_lastPosBoss, 0.01f));
             loot.Append(m_upgrade.transform.DOShakePosition(5.0f, new Vector3(0.0f, -0.05f, 0.0f), 2, 40.0f, false, false).SetLoops(1000));
             loot.Play();
 
-            m_bossFin = true;
+            
+        }
+
+        if(m_bossActive == true)
+        {
+            m_lastPosBoss = m_boss.transform.position;
         }
 
         //  test de sequence pour l'apparition de l'upgrade
@@ -58,9 +68,10 @@ public class TriggerBossStage : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(/*collision.tag == "Player" &&*/ m_bossActive == false)
+        if(collision.tag == "Player" && m_bossActive == false)
         {
             m_boss.SetActive(true);
+            m_boss.GetComponent<boss>().enabled = true;
             m_bossActive = true;
             m_Porte.GetComponent<BoxCollider2D>().enabled = true;
             //m_Porte.PlayAnimation()
