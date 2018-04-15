65797a75c802e5bc481385b9620a7cb10a2354e3
chaospat <patouk342@hotmail.fr>
2018-04-15 16:07:02 -0400

    particule contacte balle

--- Cuboid/Assets/Scripts/Shoot.cs
+++ Cuboid/Assets/Scripts/Shoot.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour {
     public GameObject bulletPrefab;
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.anyKey) {
+
+    public float fireRate = 0.2f;
+    private float attaqueCooldown;
+
+    public bool CanAttack {
+        get {
+            return attaqueCooldown <= 0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (attaqueCooldown > 0)
+            attaqueCooldown -= Time.deltaTime;
+
+        if (Input.anyKey && CanAttack) {
             Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+            attaqueCooldown = fireRate;
         }
 	}
 }
