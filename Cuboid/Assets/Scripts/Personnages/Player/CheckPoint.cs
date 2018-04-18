088a5c1329d01b3fbc8a5eda339682d27e26de5e
Gabriel-PC <gab_sim16@hotmail.com>
2018-04-18 16:24:08 -0400

    checkpoint + tuto

--- Cuboid/Assets/Scripts/Personnages/Player/CheckPoint.cs
+++ Cuboid/Assets/Scripts/Personnages/Player/CheckPoint.cs
@@ -6,11 +6,13 @@ public class CheckPoint : MonoBehaviour {
 
     private Animator m_anim;
     private bool m_currentState = false;
+    private bool m_endGameState = false;
 
 	// Use this for initialization
 	void Start () {
         m_anim = GetComponent<Animator>() as Animator;
         m_anim.GetComponent<Animator>().SetBool("CurrentSpawner", false);
+        
     }
 	
 	// Update is called once per frame
@@ -20,13 +22,33 @@ public class CheckPoint : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && m_endGameState == false)
         {
             if (m_currentState == false)
             {
+                foreach (Transform check in transform.parent)
+                {
+                    if(check != transform)
+                    {
+                        Debug.Log(check.name);
+                        check.GetComponent<Animator>().SetBool("CurrentSpawner", false);
+                        check.GetComponent<CheckPoint>().m_currentState = false;
+                    }
+                }
+
+
                 m_anim.GetComponent<Animator>().SetBool("CurrentSpawner", true);
                 m_currentState = true;
+                GameMaster.SetSpawnPlayer(transform);
+
             }
         }
+    }
+
+    public void EndGameState()
+    {
+        m_endGameState = true;
+        m_currentState = false;
+        m_anim.GetComponent<Animator>().SetBool("CurrentSpawner", false);
     }
 }
